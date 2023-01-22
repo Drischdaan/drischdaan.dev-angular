9a99aa265a741db667675c8dbb480323d4ee65fa
Drischdaan <lpboy100@gmail.com>
2023-01-22 23:23:35 +0100

    chore: updates inputs of project card component

--- src/app/components/project-card/project-card.component.ts
+++ src/app/components/project-card/project-card.component.ts
@@ -1,18 +1,16 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { Component, ElementRef, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
   styleUrls: ['./project-card.component.scss']
 })
-export class ProjectCardComponent implements OnInit {
+export class ProjectCardComponent {
 
-  card: ElementRef = new ElementRef('card');
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() image: string = '';
 
   constructor() { }
-
-  ngOnInit(): void {
-    console.log(this.card);
-  }
 
 }
