1abc7da3532fda2b60462367515a89ff293dc816
Drischdaan <lpboy100@gmail.com>
2023-01-24 00:08:02 +0100

    chore: adds drischdaan logo to mobile menu

--- src/app/components/navigation-bar/navigation-bar.component.ts
+++ src/app/components/navigation-bar/navigation-bar.component.ts
@@ -12,7 +12,7 @@ export class NavigationBarComponent {
 
   @Input() items: INavigationItem[] = [];
 
-  mobileMenuOpen$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  mobileMenuOpen$: BehaviorSubject<boolean> = new BehaviorSubject(true);
 
   constructor(
     private readonly router: Router
