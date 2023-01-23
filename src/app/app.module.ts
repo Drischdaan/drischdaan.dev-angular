b6f0ced4cba7e95f4a47bb7494df5171de2ce2e0
Drischdaan <lpboy100@gmail.com>
2023-01-23 19:22:56 +0100

    chore: adds navigation bar and font

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProjectCardComponent } from './components/project-card/project-card.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectCardComponent
+    ProjectCardComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule
