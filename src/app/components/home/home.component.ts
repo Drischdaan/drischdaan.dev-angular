635d91b418713fa67aafd0298148ba72c3567000
Drischdaan <lpboy100@gmail.com>
2023-01-24 19:20:17 +0100

    chore: adds home component

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+
+  constructor() { }
+
+  public getAge(): number {
+    const today: Date = new Date();
+    const birthDate: Date = new Date('2003/06/23'); // Leaked my birthday lol
+    let age: number = today.getFullYear() - birthDate.getFullYear();
+    const month: number = today.getMonth() - birthDate.getMonth();
+    if (month < 0 || (month === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  }
+
+}
